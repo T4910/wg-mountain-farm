2101e0d25c56dba7120f9ad3b60cfb86330e0da6
T4910 <ttaiwo4910@gmail.com>
2025-08-12 13:41:03 +0100

    🚨 fix: comment out unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Footer from "@/components/LandingPage/Footer";
 import Gallery from "@/components/LandingPage/Gallery";
 import Hero from "@/components/LandingPage/Hero";
 import Impact from "@/components/LandingPage/Impact";
-import Journey from "@/components/LandingPage/Journey";
+// import Journey from "@/components/LandingPage/Journey";
 import MissionVision from "@/components/LandingPage/MissionVision";
 import Services from "@/components/LandingPage/Services";
-import Testimonials from "@/components/LandingPage/Testimonials";
+// import Testimonials from "@/components/LandingPage/Testimonials";
 
 export default function Home() {
   return (
