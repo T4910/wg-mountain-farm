c366bd1d518378348e3d77731991b2ca55f7dd87
T4910 <ttaiwo4910@gmail.com>
2025-08-12 13:38:07 +0100

    ✨ feat: connect all sections of landing page together

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+import AboutUs from "@/components/LandingPage/AboutUs";
+import Contact from "@/components/LandingPage/Contact";
+import Footer from "@/components/LandingPage/Footer";
+import Gallery from "@/components/LandingPage/Gallery";
 import Hero from "@/components/LandingPage/Hero";
+import Impact from "@/components/LandingPage/Impact";
+import Journey from "@/components/LandingPage/Journey";
+import MissionVision from "@/components/LandingPage/MissionVision";
+import Services from "@/components/LandingPage/Services";
+import Testimonials from "@/components/LandingPage/Testimonials";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <AboutUs />
+      <MissionVision />
+      <Services />
+      <Impact />
+      {/* <Testimonials />
+      <Journey /> */}
+      <Gallery />
+      <Contact />
+      <Footer />
     </main>
   );
 }
