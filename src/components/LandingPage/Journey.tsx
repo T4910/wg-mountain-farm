52907312db31b143ecc93315e185ce6fd0aa0a30
T4910 <ttaiwo4910@gmail.com>
2025-08-13 10:04:01 +0100

    📱 feat: enhance layout and styling for Journey section

--- src/components/LandingPage/Journey.tsx
+++ src/components/LandingPage/Journey.tsx
@@ -4,9 +4,9 @@ import React, { ReactNode } from "react";
 
 export default function Journey() {
   return (
-    <section className="py-16 px-12 bg-gray-50 overflow-hidden max-w-8xl mx-auto">
+    <section className="px-4 md:px-12 py-16 md:py-28 bg-gray-50 overflow-hidden max-w-8xl mx-auto">
       {/* Header */}
-      <div className="mb-20 lg:mb-4 max-w-xl">
+      <div className="mb-12 md:mb-20 lg:mb-4 max-w-xl">
         <div className="title-header">How We Work</div>
         <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-black leading-tighter mb-4">
           Our Journey from Grass to Greatness
@@ -24,8 +24,8 @@ export default function Journey() {
           <SVGline />
         </div>
 
-        <div className="grid grid-cols-6 grid-rows-6 gap-x-6 lg:gap-12">
-          <div className="col-span-2 row-span-4 col-start-1 row-start-3">
+        <div className="grid md:grid-cols-6 md:grid-rows-6 max-sm:gap-y-4 gap-x-6 lg:gap-12">
+          <div className="max-md:bg-white max-md:border-primary/30 max-md:border-2 max-md:rounded-md max-md:p-6 max-md:pt-12 max-md:pb-8 md:col-span-2 md:row-span-4 md:col-start-1 md:row-start-3">
             <StepCard
               number={1}
               title="Nurture Pastures"
@@ -33,7 +33,7 @@ export default function Journey() {
               description="We carefully cultivate nutritious grasslands using sustainable practices, ensuring optimal soil health and biodiversity that provides premium nutrients for our animals throughout the year."
             />
           </div>
-          <div className="col-span-2 row-span-4 col-start-3 row-start-2">
+          <div className="max-md:bg-white max-md:border-primary/30 max-md:border-2 max-md:rounded-md max-md:p-6 max-md:pt-12 max-md:pb-8 md:col-span-2 md:row-span-4 md:col-start-3 md:row-start-2">
             <StepCard
               number={2}
               title="Raise Livestock"
@@ -41,12 +41,12 @@ export default function Journey() {
               description="Our cattle, sheep, and goats are raised in open, well‑managed fields where they roam freely, graze on nutritious grass, and receive proactive health care to maintain peak wellness and quality."
             />
           </div>
-          <div className="col-span-2 row-span-4 col-start-5 row-start-1">
+          <div className="max-md:bg-white max-md:border-primary/30 max-md:border-2 max-md:rounded-md max-md:p-6 max-md:pt-12 max-md:pb-8 md:col-span-2 md:row-span-4 md:col-start-5 md:row-start-1">
             <StepCard
               number={3}
               title={
                 <span>
-                  Deliver Quality <br /> Products
+                  Deliver Quality <br className="max-md:hidden" /> Products
                 </span>
               }
               icon={BadgeCheck}
@@ -68,9 +68,9 @@ type StepCardProps = {
 
 function StepCard({ number, title, description, icon: Icon }: StepCardProps) {
   return (
-    <div className="relative px-0 lg:p-6  bg-whte">
+    <div className="relative px-0 lg:p-6">
       {/* Floating icon square with green diagonal accent */}
-      <div className="absolute -top-3 left-6">
+      <div className="absolute -top-6 lg:-top-3 lg:left-6">
         {/* green diagonal accent behind */}
         <div className="absolute -z-10 -left-2 -top-2 w-6 h-6 bg-primary rotate-45 rounded-sm" />
         <div className="w-10 h-10 rounded-lg bg-blue-50 flex items-center justify-center">
@@ -79,7 +79,7 @@ function StepCard({ number, title, description, icon: Icon }: StepCardProps) {
       </div>
 
       {/* Big faded step number */}
-      <div className="pointer-events-none select-none absolute left-[calc(100%-6rem)] -top-36 lg:-top-24 text-6xl md:text-[11rem] font-bold text-primary/60">
+      <div className="pointer-events-none select-none absolute left-[calc(100%-5rem)] lg:left-[calc(100%-6rem)] -top-12 md:-top-36 lg:-top-32 text-[12rem] md:text-[9rem] lg:text-[11rem] font-bold text-primary/20 md:text-primary/60">
         {number}
       </div>
 
